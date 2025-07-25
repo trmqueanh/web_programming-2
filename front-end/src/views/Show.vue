681197d0254d3af913d8fd4515cc39d2c2540a95
trmqueanh <trmqueanh@gmail.com>
2025-07-25 11:35:11 +0700

    Re-add front-end as normal folder (not submodule)

---
+++ front-end/src/views/Show.vue
@@ -0,0 +1,69 @@
+<template>
+    <div>
+      <h1>Show Word</h1>
+  
+          <div class="ui labeled input fluid">
+      <div class="ui label">
+        <i class="united kingdom flag"></i> English
+      </div>
+      <input type="text" readonly :value="word.english" />
+    </div>
+
+    <div class="ui labeled input fluid">
+      <div class="ui label">
+        <i class="germany flag"></i> German
+      </div>
+      <input type="text" readonly :value="word.german" />
+    </div>
+
+    <div class="ui labeled input fluid">
+      <div class="ui label">
+        <i class="vietnam flag"></i> Vietnamese
+      </div>
+      <input type="text" readonly :value="word.vietnamese" />
+    </div>
+    <div class="actions">
+      <router-link :to="{ name: 'edit', params: { id: $route.params.id }}">
+        Edit word
+      </router-link>
+    </div>
+
+    </div>
+  </template>
+  
+  <script lang="ts">
+  import { api } from '../helpers/helpers';
+
+  export default {
+    name: 'show',
+    data() {
+  return {
+    word: {
+      english: '',
+      german: '',
+      vietnamese:''
+    }
+  };
+},
+
+    async mounted (){
+        this.word = await api.getWord(this.$route.params.id);
+    }
+  };
+  
+  </script>
+  
+  <style scoped>
+  .actions a {
+    display: block;
+    text-decoration: underline;
+    margin: 20px 10px;
+  }
+
+  .ui.label {
+  min-width: 140px;
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+}
+  </style>
---
+++ front-end/src/views/Test.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <h1>Quiz Test</h1>
+
+    <div v-if="words.length < 5">
+      <p>You need to enter at least five words to begin the test</p>
+    </div>
+    <div v-else>
+      <!-- 🟦 Chọn ngôn ngữ cần kiểm tra -->
+      <div class="ui form" v-if="!startTest">
+        <div class="field">
+          <label>Select the language you want to be tested on:</label>
+          <select v-model="testLang" class="ui dropdown">
+            <option disabled value="">Please select</option>
+            <option value="english">English</option>
+            <option value="german">German</option>
+          </select>
+        </div>
+        <button class="ui primary button" :disabled="!testLang" @click="startTest = true">
+          Start Test
+        </button>
+      </div>
+
+      <!-- 🟩 Bắt đầu bài kiểm tra -->
+      <vocab-test
+        v-if="startTest"
+        :words="words"
+        :lang="testLang"
+        :baseLang="'vietnamese'"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import { api } from '../helpers/helpers';
+import VocabTest from '../components/VocabTest.vue';
+
+export default {
+  name: 'test',
+  components: {
+    'vocab-test': VocabTest
+  },
+  data() {
+    return {
+      words: [],
+      testLang: '',    // Ngôn ngữ được chọn
+      startTest: false // Bắt đầu chưa
+    };
+  },
+  async mounted() {
+    this.words = await api.getWords();
+  }
+};
+</script>
