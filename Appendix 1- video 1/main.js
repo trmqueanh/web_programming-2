1dc539ca157e355ad5caef945e3b5d98001373ca
trmqueanh <trmqueanh@gmail.com>
2025-08-04 05:17:21 +0700

    Add Appendix 1 - video 1

---
+++ Appendix 1- video 1/main.js
@@ -0,0 +1,10 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        //task
+      product: 'Boots',
+      //challenge
+      description: 'A pair of warm, fuzzy socks'
+    } 
+  })
+  //Tran Mai Que Anh
